ea39a087019a751a256718bd561bb280ce42d909
Mostafa <mostafa.sedaghat@gmail.com>
2024-10-31 20:30:50 +0800

    update kotlin test

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/pactus/TestPactusSigner.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/pactus/TestPactusSigner.kt
@@ -27,15 +27,16 @@ class TestPactusSigner {
     fun testPactusTransactionSigning() {
         val signingInput = Pactus.SigningInput.newBuilder()
         signingInput.apply {
-            privateKey = ByteString.copyFrom(PrivateKey("4e51f1f3721f644ac7a193be7f5e7b8c2abaa3467871daf4eacb5d3af080e5d6".toHexByteArray()).data())
+            privateKey = ByteString.copyFrom(
+                PrivateKey("4e51f1f3721f644ac7a193be7f5e7b8c2abaa3467871daf4eacb5d3af080e5d6".toHexByteArray()).data())
             transaction = Pactus.TransactionMessage.newBuilder().apply {
-                lockTime = 0x00030201
-                fee = 1000
-                memo = "test"
+                lockTime = 2335524
+                fee = 10000000
+                memo = "wallet-core"
                 transfer = Pactus.TransferPayload.newBuilder().apply {
                     sender = "pc1rwzvr8rstdqypr80ag3t6hqrtnss9nwymcxy3lr"
                     receiver = "pc1r0g22ufzn8qtw0742dmfglnw73e260hep0k3yra"
-                    amount = 20000
+                    amount = 200000000
                 }.build()
             }.build()
         }
@@ -43,17 +44,17 @@ class TestPactusSigner {
         val output = AnySigner.sign(signingInput.build(), PACTUS, SigningOutput.parser())
 
         assertEquals(
-            "0x34cd4656a98f7eb996e83efdc384cefbe3a9c52dca79a99245b4eacc0b0b4311",
+            "0x1b6b7226f7935a15f05371d1a1fefead585a89704ce464b7cc1d453d299d235f",
             Numeric.toHexString(output.transactionId.toByteArray())
         )
 
         assertEquals(
-            "0x50ac25c7125271489b0cd230549257c93fb8c6265f2914a988ba7b81c1bc47fff027412dd59447867911035ff69742d171060a1f132ac38b95acc6e39ec0bd09",
+            "0x8dea6d79ee7ec60f66433f189ed9b3c50b2ad6fa004e26790ee736693eda850695794161374b22c696dabb98e93f6ca9300b22f3b904921fbf560bb72145f4fa",
             Numeric.toHexString(output.signature.toByteArray())
         )
 
         assertEquals(
-            "0x000101020300e807047465737401037098338e0b6808119dfd4457ab806b9c2059b89b037a14ae24533816e7faaa6ed28fcdde8e55a7df21a09c0150ac25c7125271489b0cd230549257c93fb8c6265f2914a988ba7b81c1bc47fff027412dd59447867911035ff69742d171060a1f132ac38b95acc6e39ec0bd0995794161374b22c696dabb98e93f6ca9300b22f3b904921fbf560bb72145f4fa",
+            "0x000124a3230080ade2040b77616c6c65742d636f726501037098338e0b6808119dfd4457ab806b9c2059b89b037a14ae24533816e7faaa6ed28fcdde8e55a7df218084af5f4ed8fee3d8992e82660dd05bbe8608fc56ceabffdeeee61e3213b9b49d33a0fc8dea6d79ee7ec60f66433f189ed9b3c50b2ad6fa004e26790ee736693eda850695794161374b22c696dabb98e93f6ca9300b22f3b904921fbf560bb72145f4fa",
             Numeric.toHexString(output.signedTransactionData.toByteArray())
         )
     }
